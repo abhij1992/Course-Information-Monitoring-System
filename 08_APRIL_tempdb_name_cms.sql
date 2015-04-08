7e217ad84fe58d63a7f13f6ae720ffeb641af26a
lazyrunner <joshua.mark92@gmail.com>
2015-04-08 18:30:49 +0530

    Database Structure
    created DB structure

---
+++ 08_APRIL_tempdb_name_cms.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 08, 2015 at 02:59 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE IF NOT EXISTS `faculty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(30) NOT NULL,
+  `pword` varchar(18) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `isAdmin` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `uname`, `pword`, `name`, `isAdmin`) VALUES
+(1, 'josh', 'josh', 'joshua', 0),
+(2, 'admin', 'admin', 'administrator', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `heading` varchar(40) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `subject_id` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `progress`
+--
+
+CREATE TABLE IF NOT EXISTS `progress` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `chapter_no` int(3) NOT NULL,
+  `unit_no` int(3) NOT NULL,
+  `text` varchar(50) NOT NULL,
+  `estimated_hrs` int(3) NOT NULL,
+  `completed_hrs` int(3) NOT NULL,
+  `is_complete` int(3) NOT NULL,
+  `subject_id` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE IF NOT EXISTS `subject` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `subject_code` varchar(15) NOT NULL,
+  `section` varchar(4) NOT NULL,
+  `subject_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
