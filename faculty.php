007de349a9f872996e5a927897c2aece9bc8bbbc
lazyrunner <joshua.mark92@gmail.com>
2015-04-21 10:32:23 +0530

    small change in faculty from github sync

--- faculty.php
+++ faculty.php
@@ -316,21 +316,4 @@ Panelo - Free Admin Template by <a href="htpp://csstemplatesmarket.com" target="
     	
 </body>
 </html>
-=======
-<?php
-session_start();
-if(isset($_SESSION['unames']))
-{
-  if($_SESSION['isAdmin']!=0)
-   {
-     header('Location: index.php');
-   }   
-}
-else header('Location: index.php');
-?>
 
-
-FACULTY <a href="logout.php" >Logout</a>
-
-<h2>Welcome <?php echo $_SESSION['name']; ?></h2>
->>>>>>> origin/master
